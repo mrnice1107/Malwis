afac22a3b46c157e5072e0d865e31cedc4d9454b
mrnice1107 <55033883+mrnice1107@users.noreply.github.com>
2022-09-28 18:06:03 +0200

    test project, email filter
    email validation
    + function to check if text is email
    
    + console project
    + unittest project
    + test for dictionary extention
    ( blank test so far... )

--- Malwis/Extentions/Dictionary/DictionaryExtentions.cs
+++ Malwis/Extentions/Dictionary/DictionaryExtentions.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections;
+using System.Text;
 
 namespace Malwis.Extentions.Dictionary;
 public static class DictionaryExtentions
@@ -39,31 +40,69 @@ public static class DictionaryExtentions
         return result;
     }
 
-    public static string ToStringFancy<TKey, TValue>(this IDictionary<TKey, TValue> source)
+    public static string ToFancyString<TKey, TValue>(this IDictionary<TKey, TValue>? source, char separator = ',', bool doWhiteSpaces = false, bool doNewLines = false)
     {
         if (source.IsNullOrEmpty())
         {
             return emptyDictionary;
         }
 
-        //TODO -> {"str1": 0, "str2": 1, "str3": 2, "str4": 3} smth like that
+        const string nullStr = "NULL";
+        string whiteSpace = doWhiteSpaces ? " " : string.Empty;
 
-        return source.ToString() ?? emptyDictionary;
+        StringBuilder builder = new();
+        builder.Append('{');
+        if (doNewLines)
+        {
+            builder.AppendLine();
+        }
+        else
+        {
+            builder.Append(whiteSpace);
+        }
+
+        IEnumerator<KeyValuePair<TKey, TValue>> enumerator = source!.GetEnumerator();
+        for (int i = 0; enumerator.MoveNext(); i++)
+        {
+            KeyValuePair<TKey, TValue> pair = enumerator.Current;
+
+            builder.Append(
+                pair.Key is null ? nullStr :
+                pair.Key is string str ? $"\"{pair.Value}\"" :
+                pair.Key is IDictionary<object, object> keyDict ? keyDict.ToFancyString() :
+                pair.Key.ToString()
+                );
+
+            builder.Append(':');
+
+            builder.Append(whiteSpace);
+
+            builder.Append(
+                pair.Value is null ? nullStr :
+                pair.Value is string ? $"\"{pair.Value}\"" :
+                pair.Value is IDictionary<object, object> valueDict ? valueDict.ToFancyString() :
+                pair.Value.ToString()
+                );
+
+            if (i < source.Count -1)
+            {
+                builder.Append(separator);
+            }
+            if (doNewLines)
+            {
+                builder.AppendLine();
+            }
+            else
+            {
+                builder.Append(whiteSpace);
+            }
+        }
+
+        builder.Append('}');
+
+        return builder.ToString();
     }
 
-    public static bool IsNullOrEmpty(this IDictionary source) => source is null || source.Count == 0;
-    public static bool IsNullOrEmpty<TKey, TValue>(this IDictionary<TKey, TValue> source) => source is null || source.Count == 0;
+    public static bool IsNullOrEmpty(this IDictionary? source) => source is null || source.Count == 0;
+    public static bool IsNullOrEmpty<TKey, TValue>(this IDictionary<TKey, TValue>? source) => source is null || source.Count == 0;
 }
-
-/*
-v1:
-
-{"str1": 0, "str2": 1, "str3": 2, "str4": 3}
--> 
-{0: "str1", 1: "str2", 2: "str3", 3: "str4"}
-
-v2:
-
-{"str1": [0,1,2], "str2":[1,2,3]} 
-{0:["str1"],1:["str1","str2"],2:["str1","str2"],3:["str2"]}
- */
---
+++ MalwisTest/EmailValidatorTest.cs
@@ -0,0 +1,19 @@
+﻿using Malwis.General.Texts.Validators;
+
+namespace MalwisTest;
+
+public class EmailValidatorTest
+{
+    [Theory]
+    [InlineData("testmail@gmail.com",true)]
+    [InlineData("randomtext",false)]
+    [InlineData("invalid email@test.com",false)]
+    [InlineData("text with email@test.com somewhere",false)]
+    [InlineData("valid.abc-mail@test.com",true)]
+    public void EmailValidator_IsMail(string email, bool expectedResult)
+    {
+        bool actual = EmailValidator.IsEmail(email);
+
+        Assert.Equal(expectedResult, actual);
+    }
+}
