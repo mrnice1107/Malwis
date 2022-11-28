4607a2d2a27ee6aa3665d9c74e3f0aae70d10c1a
mrnice1107 <55033883+mrnice1107@users.noreply.github.com>
2022-11-28 18:24:41 +0100

    update to .net7

--- test/MalwisTest/Dictionary/IsNullOrEmpty.cs
+++ test/MalwisTest/Dictionary/IsNullOrEmpty.cs
@@ -4,17 +4,14 @@ namespace MalwisTest.Dictionary;
 
 public class IsNullOrEmpty
 {
-    private static readonly IDictionary<string, string> NullDictionary = null;
     private static readonly IDictionary<string, string> EmptyDictionary = new Dictionary<string, string>();
-
     private static readonly IDictionary<string, string> NotEmptyDictionary = new Dictionary<string, string>
     {
         {"key", "value"}
     };
 
-
     [Fact]
-    public void IsNullOrEmpty_IsNull() => Assert.True(DictionaryExtensions.DictionaryIsNullOrEmpty(NullDictionary));
+    public void IsNullOrEmpty_IsNull() => Assert.True(DictionaryExtensions.DictionaryIsNullOrEmpty(null));
 
     [Fact]
     public void IsNullOrEmpty_IsEmpty() => Assert.True(DictionaryExtensions.DictionaryIsNullOrEmpty(EmptyDictionary));
